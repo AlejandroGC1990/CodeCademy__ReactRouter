15b623e66354854e4ffd077d0c98a8530d1e9f01
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-07-23 22:56:18 +0200

    useNavigate

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Footer() {
 
-  // get the navigate function
+  const navigate = useNavigate();
 
   const goBack = () => {
-    // imperatively redirect back
-    
+    navigate(-1);    
   }
 
   const goForward = () => {
-    // imperatively redirect forward
-    
+    navigate(1);
   }
 
   return (
