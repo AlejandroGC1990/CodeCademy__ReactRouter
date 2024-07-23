33439a94a7e95cc037cb21c82f7dc7eb76cf6378
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-07-23 23:03:48 +0200

    query parameters

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -8,9 +8,9 @@ export default function Articles () {
   const articles = useSelector(selectArticles);
 
   // Grab URLSearchParams object from useSearchParams hook
-  
+  const [searchParams, setSearParams] = useSearchParams();
   // Get the queryParams from object returned from useSearchParams and set to `title`
-  const title = '';
+  const title = searchParams.get("title");
 
   const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles);
 
