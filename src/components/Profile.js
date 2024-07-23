dff382d72479c089db2c11abe32d004ef6840a35
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-07-23 22:47:12 +0200

    Navigate

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, Navigate } from "react-router-dom";
 import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
 
 export default function Profile () {
   const currentUser = useSelector(selectCurrentUser)
   const loggedIn = useSelector(selectIsLoggedIn);
   
-  // use loggedIn to return a Navigate
-
+  if (!loggedIn) {
+    return <Navigate to="/sign-up"/>  
+  }
+  
   return (
     <main>
       <h1>{currentUser.username}</h1>
