b4d707ae8a4e4248dcfd9cadee864dd0ced062a4
AlexOscar <alejandro.garcia.carmona@gmail.com>
2024-07-23 18:31:30 +0200

    Basic Routing with <Route>

--- src/app/App.js
+++ src/app/App.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import Root from "../components/Root";
-// import About from "../components/About";
-// import SignUp from "../components/SignUp";
-// import Articles from "../components/Articles";
+import About from "../components/About";
+import SignUp from "../components/SignUp";
+import Articles from "../components/Articles";
 // import Article from "../components/Article";
-// import Categories from "../components/Categories";
+import Categories from "../components/Categories";
 // import Category from "../components/Category";
 // import Author from "../components/Author";
-// import Profile from "../components/Profile";
+import Profile from "../components/Profile";
 // import EditProfileForm from "../components/EditProfileForm";
 import "./App.css";
 
 
 const router = createBrowserRouter( createRoutesFromElements(
   <Route path="/" element={<Root />}>
-    {/* Add Routes here! */}
+    <Route path="/about" element={About} />
+    <Route path="/articles" element={Articles} />
+    <Route path="/categories" element={Categories} />
+    <Route path="/profile" element={Profile} />
+    <Route path="/sing-up" element={SignUp} />
   </Route>
 ));
 
