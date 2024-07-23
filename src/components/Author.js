9bfc84665dedeb90cd8926aa28f84fa738cebb8e
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-07-23 20:42:43 +0200

    useParams

--- src/components/Author.js
+++ src/components/Author.js
@@ -1,29 +1,29 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectAuthors } from "../features/authors/authorsSlice";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 // Import useParams from React Router
 
-export default function Author () {
-
-  // Extract the `name` URL parameter.
-
-  const authors = useSelector(selectAuthors)
-  const author = authors["replace me"]
+export default function Author() {
+  const { name } = useParams();
+  const authors = useSelector(selectAuthors);
+  const author = authors[name];
 
   return (
     <main>
-      <h1>Articles by REPLACE ME</h1>
+      <h1>Articles by {name}</h1>
       <ul>
-        {author && author.articles ? author.articles.map(slug => (
-          <li key={slug}>
-            <Link to={`/articles/${slug}`}>
-              {slug}
-            </Link>
-          </li>
-        )) : <p> No Articles Found...</p>}
+        {author && author.articles ? (
+          author.articles.map((slug) => (
+            <li key={slug}>
+              <Link to={`/articles/${slug}`}>{slug}</Link>
+            </li>
+          ))
+        ) : (
+          <p> No Articles Found...</p>
+        )}
       </ul>
     </main>
-  )
+  );
 }
