1317b806e5e3e0190b46b0ec8a476737832dfe6a
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-07-23 22:38:14 +0200

    nested routes

--- src/app/App.js
+++ src/app/App.js
@@ -6,22 +6,26 @@ import SignUp from "../components/SignUp";
 import Articles from "../components/Articles";
 import Article from "../components/Article";
 import Categories from "../components/Categories";
-// import Category from "../components/Category";
+import Category from "../components/Category";
 import Author from "../components/Author";
 import Profile from "../components/Profile";
-// import EditProfileForm from "../components/EditProfileForm";
+import EditProfileForm from "../components/EditProfileForm";
 import "./App.css";
 
 
 const router = createBrowserRouter( createRoutesFromElements(
-  <Route path="/" element={<Root/>}>
-    <Route path="/about" element={<About/>} />
-    <Route path="/articles" element={<Articles/>} />
-    <Route path="/articles/:title" element={<Article/>} />
-    <Route path="/authors/:name" element={<Author/>} />
-    <Route path="/categories" element={<Categories/>} />
-    <Route path="/profile" element={<Profile/>} />
-    <Route path="/sing-up" element={<SignUp/>} />
+  <Route path="/" element={ <Root/> }>
+    <Route path="/about" element={ <About/> } />
+    <Route path="/articles" element={ <Articles/> } />
+    <Route path="/articles/:title" element={ <Article/> } />
+    <Route path="/authors/:name" element={ <Author/> } />
+    <Route path="/categories" element={ <Categories/> } >
+      <Route path=":name" element={ <Category/> } />
+    </Route>
+    <Route path="/profile" element={ <Profile/> }>
+      <Route path="edit" element={ <EditProfileForm /> } />
+    </Route>
+    <Route path="/sign-up" element={<SignUp />} />
   </Route>
 ));
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
 import { signUp } from "../features/session/sessionSlice"
 import { useDispatch } from "react-redux";
-// import useNavigate
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp () {
   const [username, setUsername] = useState("");
   const dispatch = useDispatch();
-
-   // Grab the navigate function
+  const navigate = useNavigate();
 
   const handleSubmit = e => {
     e.preventDefault();
     dispatch(signUp({username: username}));
-    // imperatively redirect the user to /profile
+    navigate("/profile");
   }
 
   return (
